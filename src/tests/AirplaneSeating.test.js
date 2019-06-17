8fe43be02fb2cb0a54ddc968f21c56781d5878a2
Muhamad Risman <real.risman@gmail.com>
2019-06-18 00:02:26 +0700

    Fix identation AirplaneSeating test

--- src/tests/AirplaneSeating.test.js
+++ src/tests/AirplaneSeating.test.js
@@ -3,25 +3,25 @@ import AirplaneSeating from "./../AirplaneSeating";
 describe("Validate Input for seats", () => {
   it("should throw if input is empty array", () => {
     expect(() => {
-        new AirplaneSeating([]);
+      new AirplaneSeating([]);
     }).toThrow();
   });
 
   it("should throw if input is not a 2d array", () => {
     expect(() => {
-        new AirplaneSeating([1, 2]);
+      new AirplaneSeating([1, 2]);
     }).toThrow();
   });
 
   it("should not throw if input is a 2d array with numbers", () => {
     expect(() => {
-        new AirplaneSeating([1, 2], [3, 4]);
+      new AirplaneSeating([1, 2], [3, 4]);
     }).toBeTruthy();
   });
 
   it("should throw if input is a 2d array with non-numbers", () => {
     expect(() => {
-        new AirplaneSeating([1, 2], ["2", 3])
+      new AirplaneSeating([1, 2], ["2", 3]);
     }).toThrow();
   });
 });
