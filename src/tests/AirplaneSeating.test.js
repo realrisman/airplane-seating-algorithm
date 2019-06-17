2cce3968c6767285dc47c136611b80363adb39a2
Muhamad Risman <real.risman@gmail.com>
2019-06-18 00:00:44 +0700

    Add validate input for seats test

---
+++ src/tests/AirplaneSeating.test.js
@@ -0,0 +1,27 @@
+import AirplaneSeating from "./../AirplaneSeating";
+
+describe("Validate Input for seats", () => {
+  it("should throw if input is empty array", () => {
+    expect(() => {
+        new AirplaneSeating([]);
+    }).toThrow();
+  });
+
+  it("should throw if input is not a 2d array", () => {
+    expect(() => {
+        new AirplaneSeating([1, 2]);
+    }).toThrow();
+  });
+
+  it("should not throw if input is a 2d array with numbers", () => {
+    expect(() => {
+        new AirplaneSeating([1, 2], [3, 4]);
+    }).toBeTruthy();
+  });
+
+  it("should throw if input is a 2d array with non-numbers", () => {
+    expect(() => {
+        new AirplaneSeating([1, 2], ["2", 3])
+    }).toThrow();
+  });
+});
